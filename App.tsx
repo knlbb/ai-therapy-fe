4f6343d8b52e689534fcbd163b5cd2e4ca312aaf
knlbb <123568317+knlbb@users.noreply.github.com>
2024-05-12 14:58:36 +1000

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import { useVoiceRecognition } from './hooks/useVoiceRecognition';
+import * as FileSystem from "expo-file-system"
+import { Audio } from 'expo-av';
+import { writeAudioToFile } from './utils/writeAudioToFile';
+import { playFromPath } from './utils/playFromPath';
+import { useState } from 'react';
+import { fetchAudio } from './utils/fetchAudio';
+
+Audio.setAudioModeAsync({
+  allowsRecordingIOS: false,
+  staysActiveInBackground: false,
+  playsInSilentModeIOS: true,
+  shouldDuckAndroid: true,
+  playThroughEarpieceAndroid: false,
+})
 
 export default function App() {
+
+  const [urlPath, setUrlPath] = useState("")
+  const {state, startRecognizing, stopRecognizing, destroyRecognizer} = useVoiceRecognition()
+
+  const listFiles = async () => {
+    try {
+      const result = await FileSystem.readAsStringAsync(FileSystem.documentDirectory!)
+      if (result.length > 0){
+        const filename = result[0]
+        const path = FileSystem.documentDirectory + filename
+        setUrlPath(path)
+      }
+    }catch (e) {
+      console.log(e);
+      
+    }
+  }
+
+  const handleSubmit = async () => {
+    if (!state.results[0]) return;
+    try {
+      const audioBlob = await fetchAudio(state.results[0])
+
+      const reader = new FileReader()
+      reader.onload = async (e) => {
+        if (e.target && typeof e.target.result === "string"){
+          const audioData = e.target.result.split(",")[1]
+
+          const path = await writeAudioToFile(audioData)
+
+          setUrlPath(path)
+          await playFromPath(path)
+          destroyRecognizer()
+
+        }
+      }
+
+      reader.readAsDataURL(audioBlob)
+    }catch (err){
+      console.log(err);
+      
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        {JSON.stringify(state, null, 2)}
+      </Text>
+      <Pressable
+        onPressIn={()=> {
+          console.log("Im pressed")
+          startRecognizing()
+        }}
+        onPressOut={()=>{
+          console.log("Im left")
+          stopRecognizing()
+          handleSubmit()
+        }}
+        style={{
+          width:'90%',
+          padding:20,
+          gap:10,
+          borderWidth:3,
+          alignItems:"center",
+          borderRadius:10,
+          borderColor:"lightgray"
+        }}
+      >
+        <Text>Talk to me</Text>
+      </Pressable>
+      <Button title="Re play" onPress={async () => {
+        await playFromPath(urlPath)
+      }}></Button>
     </View>
   );
 }
